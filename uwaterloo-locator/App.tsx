c957e0a9b54ba58ac29255648f18a2da48f4593d
Kanwarpal Brar <brar.kanwarpal.school@gmail.com>
2024-04-16 00:31:54 -0400

    Worked on adding map with default region

--- uwaterloo-locator/App.tsx
+++ uwaterloo-locator/App.tsx
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar as NativeStatusBar } from 'react-native';
+import { StyleSheet, Text, View, } from 'react-native';
+import MapView from 'react-native-maps';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.container} >
+      <MapView style={styles.map} region={{latitude: 43.47198, latitudeDelta: 0, longitude: -80.54396, , longitudeDelta:}}/>
+      <StatusBar style="light" hidden={false} networkActivityIndicatorVisible={true}/>
+      <View style={styles.footer}>
+        <Text>Header</Text>
+      </View>
     </View>
   );
 }
@@ -13,8 +18,25 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    flexDirection: 'column',
+    backfaceVisibility: 'hidden',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-end',
+    height: "80%",
+    backgroundColor: 'black'
   },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+    marginTop: NativeStatusBar.currentHeight ? NativeStatusBar.currentHeight + 2 : 30,
+    zIndex: 0,
+  },
+  footer: {
+    zIndex: 1,
+    backgroundColor: 'white',
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    width: '100%',
+    height: '12%',
+    padding: 10
+  }
 });
