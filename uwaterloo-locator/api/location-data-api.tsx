965cc28e327148190b0c3547d35f97122ed8aeca
Kanwarpal Brar <brar.kanwarpal.school@gmail.com>
2024-05-02 12:02:55 -0400

    Updated packages to add svg support and linting

--- uwaterloo-locator/api/location-data-api.tsx
+++ uwaterloo-locator/api/location-data-api.tsx
@@ -2,20 +2,18 @@ import { LatLng } from "react-native-maps";
 import buildings from "../constants/buildings.json";
 
 export type NamedLatLng = {
-    name: string;
-    latitude: number;
-    longitude: number;
+  name: string;
+  latitude: number;
+  longitude: number;
 };
 
 export function fetchWashroomLocations(): Array<NamedLatLng> {
-    // TODO this is currently mocked by parsing a geojson: connect to backend later
-    return buildings.features.map(
-        build =>  {
-            return {
-                name: build.properties.buildingName,
-                latitude: build.properties.latitude || 0,
-                longitude: build.properties.longitude || 0
-            }
-        }
-    );
+  // TODO this is currently mocked by parsing a geojson: connect to backend later
+  return buildings.features.map((build) => {
+    return {
+      name: build.properties.buildingName,
+      latitude: build.properties.latitude || 0,
+      longitude: build.properties.longitude || 0,
+    };
+  });
 }
