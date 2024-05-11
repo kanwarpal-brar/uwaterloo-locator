81f61da4423040944066e105d871d02459ea96bd
Kanwarpal Brar <brar.kanwarpal.school@gmail.com>
2024-05-11 19:04:10 -0400

    Encapsulated footer into seperate file (draft)

--- uwaterloo-locator/App.tsx
+++ uwaterloo-locator/App.tsx
@@ -1,14 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import CustomMap from "./components/map/custom-map";
+import Footer from "./components/home-ui/footer";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <CustomMap />
-      <View style={{ ...styles.footer }}>
-        <Text>Header</Text>
-      </View>
+      <Footer />
       <StatusBar
         style="light"
         hidden={false}
