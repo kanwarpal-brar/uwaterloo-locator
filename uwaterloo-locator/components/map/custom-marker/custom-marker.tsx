489b7eda739f0bfee9e62f669df3516b19e63e69
Kanwarpal Brar <brar.kanwarpal.school@gmail.com>
2024-11-19 03:53:02 -0500

    updated custom marker props

--- uwaterloo-locator/components/map/custom-marker/custom-marker.tsx
+++ uwaterloo-locator/components/map/custom-marker/custom-marker.tsx
@@ -1,9 +1,19 @@
 import React from "react";
 import { Marker } from "react-native-maps";
 import { NamedLatLng } from "../../../api/location-data-api";
-import CustomPin from "../custom-pin/custom-pin";
+import CustomPin, { CustomIconType } from "../custom-pin/custom-pin";
 
-function CustomMapMarker({ location }: { location: NamedLatLng }) {
+type CustomMapMarkerProps = {
+  location: NamedLatLng;
+  fill?: string;
+  icon?: CustomIconType;
+};
+
+function CustomMapMarker({
+  location,
+  fill = "#292929",
+  icon = "toilet",
+}: CustomMapMarkerProps) {
   return (
     <Marker
       coordinate={{
@@ -13,12 +23,7 @@ function CustomMapMarker({ location }: { location: NamedLatLng }) {
       title={location.name}
       tracksViewChanges={false}
     >
-      <CustomPin
-        icon={"toilet"}
-        fill="#292929"
-        stroke="black"
-        strokeWidth={0.5}
-      />
+      <CustomPin icon={icon} fill={fill} stroke="black" strokeWidth={0.5} />
     </Marker>
   );
 }
